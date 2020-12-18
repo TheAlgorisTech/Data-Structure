6b02d3d3b569e87a3a57e3158a1f1806534c4733
phoenix <TheAlgorisTech>
2020-12-18 23:41:49 +0100

    trie doc

--- automatic-lang-detection/src/trie.h
+++ automatic-lang-detection/src/trie.h
@@ -19,7 +19,25 @@ typedef struct TrieArbre {
 	bool finNoeud;
 	} TrieArbre;
 
+
+
+/// @brief Create a trie.
+/// @return On success allocated empty TrieArbre is returned.
+
 TrieArbre *arbre_vide(void);
+
+
+/// @brief Insert key in the Trie.
+/// @param ArbreTrie. The initial Trie.
+/// @param cles The key to be inserted in the Trie.
+/// @return On success return AbreTrie with additional key.
+///
+/// Note that the key or the Trie  must not be empty, and
 TrieArbre *insert(TrieArbre *racine, const char *cles);
+
+/// @brief Check if a key belong to the Trie.
+/// @param ArbreTrie. The initial Trie.
+/// @param cles The key to be checked it's existence in the Trie.
+/// @return On success true is returned.
 bool search(TrieArbre *racine, const char *cles);
 
